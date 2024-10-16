a6d4f5f0bd211b52eb0535a1078dc2b492815403
The_phantom walker <Neocoinkitt@gmail.com>
2024-10-16 13:39:47 +0000

    installed reactbootstrap and imported some button thingy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button variant="primary">Primary</Button>
       </header>
     </div>
   );
